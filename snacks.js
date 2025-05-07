4f84c4861fc3cb08577b096af6a6df41f96e2a37
ritaurbani <ritaurbani@gmail.com>
2025-05-07 14:59:10 +0200

    snack7

--- snacks.js
+++ snacks.js
@@ -8,21 +8,75 @@ function getInitials(wholeName) {
 
 //2.
 //La funzione createSlug restituisce una stringa in lowercase.
-function createSlug(str) {
-    return str.toLowerCase()
-}
+// function createSlug(str) {
+//     return str.toLowerCase()
+// }
 
 //3.
 //"La funzione average calcola la media aritmetica di un array di numeri."
-function average(arr){
+function average(arr) {
     let sum = 0
     arr.forEach((n) => {
-        sum += n
+        if (isNaN(n)) {
+            throw new Error("Average just wants numbers")
+        } else {
+            sum += n
+        }
     })
-    return sum/arr.length
+    return sum / arr.length
 }
+//Using reduce
+//return arr.reduce((acc, num) => acc + num, 0) / arr.length
+
+//4. 
+//La funzione createSlug sostituisce gli spazi con -."
+// function createSlug(str) {
+//     return str.toLowerCase().replaceAll(" ", "-")
+// }
+
+//5.
+//La funzione isPalindrome verifica se una stringa è un palindromo.
+function isPalindrome(str) {
+    let newString = ""
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i]; // oppure newString = newString + str[i];
+        if (str.trim() === newString){
+            return true
+        }
+    }
+}
+//reverse e metodo array
+// cont strInversed = str.split("").reverse().join("");
+// return str === strInversed
+
+//6
+//La funzione createSlug lancia un errore se il titolo è vuoto o non valido
+function createSlug(str) {
+    if(!str){
+        throw new Error("no empty or not valid")
+    }
+    return str.toLowerCase().replaceAll(" ", "-")
+}
+
+//7
+///Crea un array di oggetti posts, in cui ogni oggetto ha le proprietà 
+// id, title e slug.
+// Creare un test che verifichi le seguenti descrizioni:
+// "La funzione findPostById restituisce il post corretto dato l’array di post e l’id"
+// Creare uno o più test aggiuntivi che controllino che la struttura dati passati 
+// sia conforme(ogni post ha le proprietà id, title e slug, viene passato un id numerico).
+
+
+
+function findPostById(arr, id){
+    return arr.find((p) => p.id === id)
+}
+
+
 module.exports = {
     getInitials,
     createSlug,
-    average
+    average,
+    isPalindrome,
+    findPostById
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,5 +1,5 @@
 
-const { getInitials,createSlug, average } = require("./snacks.js")
+const { getInitials,createSlug, average, isPalindrome, findPostById } = require("./snacks.js")
 
 
 //1 - "La funzione getInitials restituisce le iniziali di un nome completo."
@@ -12,12 +12,59 @@ test(`La funzione getInitials restituisce le iniziali di un nome completo`, () =
 
 
 //2.La funzione createSlug restituisce una stringa in lowercase.
-test(`La funzione createSlug restituisce una stringa in lowercase`, () => {
-    expect(createSlug("CIAO RITA")).toBe("ciao rita")
-    expect(createSlug("Ciao")).toBe("ciao")
-})
+// test(`La funzione createSlug restituisce una stringa in lowercase`, () => {
+//     expect(createSlug("CIAO RITA")).toBe("ciao rita")
+//     expect(createSlug("Ciao")).toBe("ciao")
+// })
 
 //3. La funzione average calcola la media aritmetica di un array di numeri."
 test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
     expect(average([4,3,5])).toBe(4)
+    expect(() => average([4,3,"ciao"])).toThrow()
 })    
+
+//4.La funzione createSlug sostituisce gli spazi con -."
+// test(`La funzione createSlug restituisce una stringa in lowercase`, () => {
+//     expect(createSlug("CIAO RITA bella")).toBe("ciao-rita-bella")
+    
+// })
+
+//5.La funzione isPalindrome verifica se una stringa è un palindromo.
+test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
+    expect((isPalindrome("anna"))).toBeTruthy()
+    // expect((isPalindrome("anna "))).toBeTruthy()
+    expect((isPalindrome("rita"))).toBeFalsy()
+})
+
+//6.La funzione createSlug lancia un errore se il titolo è vuoto o non valido"
+test(`La funzione createSlug lancia un errore se il titolo è vuoto o non valido`, () => {
+    expect(createSlug("CIAO RITA bella")).toBe("ciao-rita-bella")
+    expect(() => createSlug("")).toThrow()//posso anche passare la stringa che mi aspetto throw abbia per msg
+    expect(() => createSlug(null)).toThrow()
+    
+})
+
+const posts = [
+    {
+        id: 1,
+        title: "introduction js",
+        slug: "introduction-js"
+    },
+    {
+        id: 2,
+        title: "study react",
+        slug: "study-react"
+    },
+    {
+        id: 3,
+        title: "study python",
+        slug: "study-python"
+    }
+]
+
+
+//7. La funzione findPostById restituisce il post corretto dato l’array di post e l’id
+//to check ogni post ha le proprietà id, title e slug, viene passato un id numerico
+test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
+    expect(findPostById(posts, 1)).toEqual({ id: 1, title: "introduction js", slug: "introduction-js"})
+})
